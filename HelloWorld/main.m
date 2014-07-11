bcb1ffe71226d5a87dad77d1df969e695636e2ce
huweitao <huweitao@192.168.3.13>
2014-07-11 12:15:35 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by huweitao on 14-7-11.
+//  Copyright (c) 2014年 huweitao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
